e1551b2f293f4323758b45bf3f8ab9990f3bd081
intranautic <intranautic@gmail.com>
2023-12-07 21:49:25 -0800

    setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,19 +18,26 @@ set_vars() {
 }
 
 if [ "$PWD" != "$HOME/dot/scripts" ]; then
-  printf "Error: execute %s from %s\n" "$0" "$PWD"
+  printf "%s: Error execute script from the "$PWD"/scripts directory\n" "$0"
   exit 1
 fi
 
 # set os environ vars determine platform
 set_vars
 
-printf "%s: Running install script\n" "$0"
+echo "$0: Running install, script will periodically prompt with sudo!"
+echo "Terrible fucking code btw, no guarantee that this wont fuck everything :3"
+read -p "Are you sure you want to continue (y/n): " prompt
+if [[ "$prompt" == "n" ]]; then
+  echo "Exiting..."
+  exit 0
+fi
+
 echo "Updating and installing dependencies"
 sudo pacman --noconfirm -Syyuu
 sudo pacman --noconfirm -S alacritty gdb git gcc cmake make pkg-config unzip \
-  rofi polybar tmux zsh python python-pip fakeroot nitrogen yay
-yay --noconfirm -S ttf-unifont
+  rofi polybar tmux zsh python python-pip fakeroot nitrogen yay which
+yay --noconfirm -S ttf-unifont betterlockscreen
 
 mkdir -p $HOME/.config
 mkdir -p $HOME/.config/i3
@@ -41,22 +48,15 @@ if [ -e "$HOME/.config/i3/config" ]; then
 fi
 cp "../i3/config" "$HOME/.config/i3/config"
 
-echo "Build and setup custom neovim config"
-if pacman -Qs "neovim" >/dev/null; then
-  sudo pacman -R "neovim"
-fi
-git clone "https://github.com/neovim/neovim" "/tmp/neovim"
-make -C "/tmp/neovim" && sudo make -C "/tmp/neovim" install
-if [ -e "$HOME/.config/nvim" ]; then
-  mv "$HOME/.config/nvim" "$HOME/.config/nvim.old"
-fi
-cp -r "../nvim" "$HOME/.config"
-
 echo "Removing potential previous configs for tmux, alacritty, polybar, rofi"
 rm -f "$HOME/.tmux.conf"
 rm -rf "$HOME/.config/alacritty"
 rm -rf "$HOME/.config/polybar"
 rm -rf "$HOME/.config/rofi"
+
+echo "Install and setup betterlockscreen"
+echo "Patching i3exit to use betterlockscreen"
+sudo cat "../i3exit" > "$(which i3exit)"
 
 echo "Installing alacritty config"
 cp -r "../alacritty" "$HOME/.config"
@@ -73,11 +73,22 @@ git clone "https://github.com/scwuaptx/Pwngdb.git" "~/Pwngdb"
 sh -c "$HOME/pwndbg/setup.sh" <<< "y" <<< "y"
 cp "../.gdbinit" "$HOME/.gdbinit"
 
-echo "Install and setup betterlockscreen"
-yay --noconfirm -S betterlockscreen
+echo "Setup and installing radare2"
+git clone https://github.com/radareorg/radare2 ~/radare2
+sh -c "$HOME/radare2/sys/install.sh"
+echo "Installing r2ghidra decompiler"
+r2pm -ci r2ghidra r2ghidra-sleigh
 
-echo "Patching i3exit to use betterlockscreen"
-sudo cat "../i3exit" > "$(which i3exit)"
+echo "Build and setup custom neovim config"
+if pacman -Qs "neovim" >/dev/null; then
+  sudo pacman -R "neovim"
+fi
+git clone "https://github.com/neovim/neovim" "/tmp/neovim"
+make -C "/tmp/neovim" && sudo make -C "/tmp/neovim" install
+if [ -e "$HOME/.config/nvim" ]; then
+  mv "$HOME/.config/nvim" "$HOME/.config/nvim.old"
+fi
+cp -r "../nvim" "$HOME/.config"
 
 echo "Script finished installing, rebooting in 5 seconds..."
 sleep 5; reboot
