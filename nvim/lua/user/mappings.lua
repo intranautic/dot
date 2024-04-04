b7e245db22590f6d57af199830f920cd2d5decaf
intranautic <intranautic@gmail.com>
2024-04-04 00:15:22 -0700

    new

--- nvim/lua/user/mappings.lua
+++ nvim/lua/user/mappings.lua
@@ -1,5 +1,5 @@
-local opts = { silent = true }
-local term_opts = { silent = true }
+local opts = { noremap=false, silent = true }
+local term_opts = { noremap=false, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 -- plugin keybinds
@@ -10,10 +10,10 @@ keymap("n", "<S-t>", ":NvimTreeToggle<CR>", opts)
 keymap("n", "<S-n>", ":bn<CR>", opts)
 keymap("n", "<S-p>", ":bp<CR>", opts)
 keymap("n", "<S-x>", ":bd<CR>", opts)
-keymap("n", "<S-h>", "<C-w>h", opts)
-keymap("n", "<S-j>", "<C-w>j", opts)
-keymap("n", "<S-k>", "<C-w>k", opts)
-keymap("n", "<S-l>", "<C-w>l", opts)
+keymap("n", "<S-Left>", "<C-w>h", opts)
+keymap("n", "<S-Down>", "<C-w>j", opts)
+keymap("n", "<S-Up>", "<C-w>k", opts)
+keymap("n", "<S-Right>", "<C-w>l", opts)
 
 -- easy keybinds for saving and quitting
 keymap("n", "<S-w>", ":w<CR>", opts)
