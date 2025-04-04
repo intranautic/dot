8e5d0b15c28b61d63580fc589ce14c533bd3a6f4
intranautic <intranautic@gmail.com>
2025-04-04 12:24:33 -0700

    everything i touch turns to waste, everything i waswte gets recycl,ed

---
+++ lvim/clangd_config.lua
@@ -0,0 +1,97 @@
+opts = {
+  servers = {
+    -- Ensure mason installs the server
+    clangd = {
+      keys = {
+        { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+      },
+      root_dir = function(fname)
+        return require("lspconfig.util").root_pattern(
+          "Makefile",
+          "configure.ac",
+          "configure.in",
+          "config.h.in",
+          "meson.build",
+          "meson_options.txt",
+          "build.ninja"
+        )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+          fname
+        ) or require("lspconfig.util").find_git_ancestor(fname)
+      end,
+      capabilities = {
+        offsetEncoding = { "utf-16" },
+      },
+      cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=iwyu",
+        "--completion-style=detailed",
+        "--function-arg-placeholders",
+        "--fallback-style=llvm",
+      },
+      init_options = {
+        usePlaceholders = true,
+        completeUnimported = true,
+        clangdFileStatus = true,
+      },
+    },
+  },
+  setup = {
+    clangd = function(_, opts)
+      local clangd_ext_opts = LazyVim.opts("clangd_extensions.nvim")
+      require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+      return false
+    end,
+  },
+}
+
+opts = function(_, opts)
+  opts.sorting = opts.sorting or {}
+  opts.sorting.comparators = opts.sorting.comparators or {}
+  table.insert(opts.sorting.comparators, 1, require("clangd_extensions.cmp_scores"))
+end
+
+
+opts = function()
+  local dap = require("dap")
+  if not dap.adapters["codelldb"] then
+    require("dap").adapters["codelldb"] = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "codelldb",
+        args = {
+          "--port",
+          "${port}",
+        },
+      },
+    }
+  end
+  for _, lang in ipairs({ "c", "cpp" }) do
+    dap.configurations[lang] = {
+      {
+        type = "codelldb",
+        request = "launch",
+        name = "Launch file",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+      },
+      {
+        type = "codelldb",
+        request = "attach",
+        name = "Attach to process",
+        pid = require("dap.utils").pick_process,
+        cwd = "${workspaceFolder}",
+      },
+    }
+  end
+end
+
+
+opts = { ensure_installed = { "codelldb" } }
+
+
--- lvim/config.lua
+++ lvim/config.lua
@@ -21,7 +21,7 @@ opt.wrap = false
 opt.linebreak = true
 opt.laststatus = 3
 
-local opts = { noremap=false, silent = true }
+local opts = { noremap = false, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 -- buffers
@@ -37,6 +37,7 @@ keymap("n", "<S-f>", ":Telescope<CR>", opts)
 keymap("n", "<S-s>", ":Gitsigns<CR>", opts)
 keymap("n", "<S-t>", ":NvimTreeToggle<CR>", opts)
 
+
 -- lvim config
 lvim.leader = "l"
 lvim.keys.normal_mode["<C-Left>"] = false
@@ -44,17 +45,53 @@ lvim.keys.normal_mode["<C-Right>"] = false
 lvim.keys.normal_mode["<C-Up>"] = false
 lvim.keys.normal_mode["<C-Down>"] = false
 lvim.keys.normal_mode["<Tab>"] = false
-lvim.log_level = "warn"
+lvim.log_level = "error"
 lvim.format_on_save = false
+
 lvim.builtin.treesitter.highlight.enable = true
-lvim.builtin.treesitter.highlight.enabled = true
-lvim.builtin.treesitter.indent = false
+lvim.builtin.treesitter.autotag.enable = true
+
+lvim.builtin.telescope.theme = "dropdown"
 lvim.builtin.cmp.active = true
+lvim.builtin.cmp.experimental.ghost_text = true
+lvim.builtin.terminal.open_mapping = "<C-t>"
+lvim.builtin["terminal"].direction = "horizontal"
+lvim.lsp.document_highlight = true
 
 lvim.plugins = {
+  "stevearc/dressing.nvim",
+  "MunifTanjim/nui.nvim",
   { "folke/tokyonight.nvim" },
   { "catppuccin/nvim" },
   { "norcalli/nvim-colorizer.lua" },
+  "p00f/clangd_extensions.nvim",
+  lazy = true,
+  config = function() end,
+  opts = {
+    inlay_hints = {
+      inline = false,
+    },
+    ast = {
+      --These require codicons (https://github.com/microsoft/vscode-codicons)
+      role_icons = {
+        type = "",
+        declaration = "",
+        expression = "",
+        specifier = "",
+        statement = "",
+        ["template argument"] = "",
+      },
+      kind_icons = {
+        Compound = "",
+        Recovery = "",
+        TranslationUnit = "",
+        PackExpansion = "",
+        TemplateTypeParm = "",
+        TemplateTemplateParm = "",
+        TemplateParamObject = "",
+      },
+    },
+  },
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
@@ -81,7 +118,7 @@ lvim.plugins = {
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      "zbirenbaum/copilot.lua",      -- for providers='copilot'
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
@@ -108,33 +145,321 @@ lvim.plugins = {
         ft = { "markdown", "Avante" },
       },
     },
+    'numToStr/Comment.nvim',
+    {
+      "p00f/clangd_extensions.nvim",
+      lazy = true,
+      config = function() end,
+      opts = {
+        inlay_hints = {
+          inline = true,
+        },
+        ast = {
+          --These require codicons (https://github.com/microsoft/vscode-codicons)
+          role_icons = {
+            type = "",
+            declaration = "",
+            expression = "",
+            specifier = "",
+            statement = "",
+            ["template argument"] = "",
+          },
+          kind_icons = {
+            Compound = "",
+            Recovery = "",
+            TranslationUnit = "",
+            PackExpansion = "",
+            TemplateTypeParm = "",
+            TemplateTemplateParm = "",
+            TemplateParamObject = "",
+          },
+        },
+      },
+    }
   }
 }
+
 lvim.colorscheme = "catppuccin-mocha"
 lvim.builtin.autopairs.active = false
 lvim.builtin.indentlines.active = true
 lvim.builtin.alpha.active = false
-lvim.builtin.cmp.active = false
+lvim.builtin.cmp.active = true
 
+-- setup
+require('Comment').setup()
 
 -- stupid fucking cmp comment newline christ
-
 vim.diagnostic.config({
-  virtual_text=false,
-  signs = false,
-  underline = false
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  float = true
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = '*',
-    callback = function()
-        vim.opt_local.formatoptions:remove('c')
-        vim.opt_local.formatoptions:remove('r')
-        vim.opt_local.formatoptions:remove('o')
-    end,
+  pattern = '*',
+  callback = function()
+    vim.opt_local.formatoptions:remove('c')
+    vim.opt_local.formatoptions:remove('r')
+    vim.opt_local.formatoptions:remove('o')
+  end,
 })
 
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_filetypes, {
   "make"
 })
+
+opts = {
+  servers = {
+    -- Ensure mason installs the server
+    clangd = {
+      keys = {
+        { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+      },
+      root_dir = function(fname)
+        return require("lspconfig.util").root_pattern(
+          "Makefile",
+          "configure.ac",
+          "configure.in",
+          "config.h.in",
+          "meson.build",
+          "meson_options.txt",
+          "build.ninja"
+        )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+          fname
+        ) or require("lspconfig.util").find_git_ancestor(fname)
+      end,
+      capabilities = {
+        offsetEncoding = { "utf-16" },
+      },
+      cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=iwyu",
+        "--completion-style=detailed",
+        "--function-arg-placeholders",
+        "--fallback-style=llvm",
+      },
+      init_options = {
+        usePlaceholders = true,
+        completeUnimported = true,
+        clangdFileStatus = true,
+      },
+    },
+  },
+  setup = {
+    clangd = function(_, opts)
+      local clangd_ext_opts = LazyVim.opts("clangd_extensions.nvim")
+      require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+      return false
+    end,
+  },
+}
+
+opts = function(_, opts)
+  opts.sorting = opts.sorting or {}
+  opts.sorting.comparators = opts.sorting.comparators or {}
+  table.insert(opts.sorting.comparators, 1, require("clangd_extensions.cmp_scores"))
+end
+
+
+opts = function()
+  local dap = require("dap")
+  if not dap.adapters["codelldb"] then
+    require("dap").adapters["codelldb"] = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "codelldb",
+        args = {
+          "--port",
+          "${port}",
+        },
+      },
+    }
+  end
+  for _, lang in ipairs({ "c", "cpp" }) do
+    dap.configurations[lang] = {
+      {
+        type = "codelldb",
+        request = "launch",
+        name = "Launch file",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+      },
+      {
+        type = "codelldb",
+        request = "attach",
+        name = "Attach to process",
+        pid = require("dap.utils").pick_process,
+        cwd = "${workspaceFolder}",
+      },
+    }
+  end
+end
+
+
+opts = { ensure_installed = { "codelldb" } }
+
+
+opts = function()
+  ---@class PluginLspOpts
+  local ret = {
+    -- options for vim.diagnostic.config()
+    ---@type vim.diagnostic.Opts
+    diagnostics = {
+      underline = true,
+      update_in_insert = false,
+      virtual_text = {
+        spacing = 4,
+        source = "if_many",
+        prefix = "●",
+        -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+        -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+        -- prefix = "icons",
+      },
+      severity_sort = true,
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
+          [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
+          [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
+          [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
+        },
+      },
+    },
+    -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
+    -- Be aware that you also will need to properly configure your LSP server to
+    -- provide the inlay hints.
+    inlay_hints = {
+      enabled = true,
+      exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
+    },
+    -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
+    -- Be aware that you also will need to properly configure your LSP server to
+    -- provide the code lenses.
+    codelens = {
+      enabled = false,
+    },
+    -- add any global capabilities here
+    capabilities = {
+      workspace = {
+        fileOperations = {
+          didRename = true,
+          willRename = true,
+        },
+      },
+    },
+    -- options for vim.lsp.buf.format
+    -- `bufnr` and `filter` is handled by the LazyVim formatter,
+    -- but can be also overridden when specified
+    format = {
+      formatting_options = nil,
+      timeout_ms = nil,
+    },
+    -- LSP Server Settings
+    ---@type lspconfig.options
+    servers = {
+      lua_ls = {
+        -- mason = false, -- set to false if you don't want this server to be installed with mason
+        -- Use this to add any additional keymaps
+        -- for specific lsp servers
+        -- ---@type LazyKeysSpec[]
+        -- keys = {},
+        settings = {
+          Lua = {
+            workspace = {
+              checkThirdParty = false,
+            },
+            codeLens = {
+              enable = true,
+            },
+            completion = {
+              callSnippet = "Replace",
+            },
+            doc = {
+              privateName = { "^_" },
+            },
+            hint = {
+              enable = true,
+              setType = false,
+              paramType = true,
+              paramName = "Disable",
+              semicolon = "Disable",
+              arrayIndex = "Disable",
+            },
+          },
+        },
+      },
+    },
+    -- you can do any additional lsp server setup here
+    -- return true if you don't want this server to be setup with lspconfig
+    ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+    setup = {
+      -- example to setup with typescript.nvim
+      -- tsserver = function(_, opts)
+      --   require("typescript").setup({ server = opts })
+      --   return true
+      -- end,
+      -- Specify * to use this function as a fallback for any server
+      -- ["*"] = function(server, opts) end,
+    },
+  }
+  return ret
+end
+
+opts = {
+  highlight = { enable = true },
+  indent = { enable = true },
+  ensure_installed = {
+    "bash",
+    "c",
+    "diff",
+    "html",
+    "javascript",
+    "jsdoc",
+    "json",
+    "jsonc",
+    "lua",
+    "luadoc",
+    "luap",
+    "markdown",
+    "markdown_inline",
+    "printf",
+    "python",
+    "query",
+    "regex",
+    "toml",
+    "tsx",
+    "typescript",
+    "vim",
+    "vimdoc",
+    "xml",
+    "yaml",
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<C-space>",
+      node_incremental = "<C-space>",
+      scope_incremental = false,
+      node_decremental = "<bs>",
+    },
+  },
+  textobjects = {
+    move = {
+      enable = true,
+      goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+      goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+      goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+      goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+    },
+  },
+}
+
+
 
